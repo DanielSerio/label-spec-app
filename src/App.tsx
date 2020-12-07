e8c56a2f8e0f1ca878bd198e57efece2fbe129bb
DanielSerio <dserio.d@gmail.com>
2020-12-07 08:53:20 -0600

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './components/containers/AppContainer';
+import theme from './styles/theme';
 
 function App() {
   return (
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <AppContainer />
+    </ThemeProvider>
     </BrowserRouter>
   );
 }
